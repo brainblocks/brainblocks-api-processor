527a9d27b32133c4b7b56e4d7f2133a7a0339fb6
Daniel Brain <daniel@bluesuncorp.co.uk>
2018-08-17 18:43:14 -0700

    Change tests to point to 127.0.0.1

--- test/config.js
+++ test/config.js
@@ -1,7 +1,7 @@
 /* @flow */
 
 export const PORT = 8000;
-export const URL = `http://localhost:${ PORT }`;
+export const URL = `http://127.0.0.1:${ PORT }`;
 
 export const NANO_PORT = 7070;
 export const NANO_SERVER = `http://127.0.0.1:${ NANO_PORT }`;
