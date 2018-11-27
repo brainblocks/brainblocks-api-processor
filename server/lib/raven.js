f51326f89e0d4f89142c70710d3dca37dd6617ca
Ty Schenk <7807169+schenkty@users.noreply.github.com>
2018-11-27 12:40:44 -0500

    added sample rate for raven logging (#12)
    * Update raven.js
    
    added sampleRate for Raven so we don't send every error to Raven.

--- server/lib/raven.js
+++ server/lib/raven.js
@@ -2,6 +2,12 @@
 
 import ravenSentry from 'raven';
 
-ravenSentry.config('https://***REMOVED***:***REMOVED***@sentry.io/287275').install();
+// ravenSentry.config('https://***REMOVED***:***REMOVED***@sentry.io/287275').install();
+
+ravenSentry.config('https://***REMOVED***:***REMOVED***@sentry.io/287275', {
+    sampleRate: 0.5 // send 50% of events, drop the other half
+}).install();
+
+ravenSentry.disableConsoleAlerts();
 
 export let raven = ravenSentry;
