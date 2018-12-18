f226abf07d982d7b332c6fac457116d81807b30a
Daniel Brain <chuntheunavoidable@gmail.com>
2018-12-18 19:45:00 +0000

    Catch promise correctly (#33)

--- clean.js
+++ clean.js
@@ -6,10 +6,8 @@ require('babel-register');
 // eslint-disable-next-line import/no-commonjs
 const { cleanTransactions } = require('./server/clean');
 
-try {
-    cleanTransactions();
-} catch (err) {
+cleanTransactions().catch(err => {
     console.error('Error in cleanTransactions()', err.stack);
     // eslint-disable-next-line no-process-exit, unicorn/no-process-exit
-    process.exit(0);
-}
+    process.exit(1);
+});
