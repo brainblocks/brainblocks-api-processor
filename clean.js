10c86eb95664150575a79dcf3710c139b4354f54
Angus Russell <angus.russell89@gmail.com>
2018-12-18 15:48:23 +1100

    Log errors when clean script fails (#32)

--- clean.js
+++ clean.js
@@ -9,6 +9,7 @@ const { cleanTransactions } = require('./server/clean');
 try {
     cleanTransactions();
 } catch (err) {
+    console.error('Error in cleanTransactions()', err.stack);
     // eslint-disable-next-line no-process-exit, unicorn/no-process-exit
     process.exit(0);
 }
