02cf8c2e40aa09a3f30b483a7dfc2307a39eab2f
Rick Allen <rick@brainblocks.io>
2018-12-31 05:41:27 +0000

    Update db-setup.sh

--- scripts/db-setup.sh
+++ scripts/db-setup.sh
@@ -69,4 +69,4 @@ CREATE TRIGGER paypal_transaction_modtime
     BEFORE UPDATE ON transaction
     FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
 
-" | psql -h postgres "$db";
+" | psql -h localhost "$db";
