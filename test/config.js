97f73e5a858d939c27a917a1238d5b379c744092
Rick Allen <contact@rickallen.me>
2018-12-31 00:15:53 -0600

    Added db host

--- test/config.js
+++ test/config.js
@@ -1,7 +1,8 @@
 /* @flow */
 
 export const PORT = 8000;
-export const URL = `http://127.0.0.1:${ PORT }`;
+export const DB_HOST = process.env['DB_HOST'] || '127.0.0.1';
+export const URL = `http://${ DB_HOST }:${ PORT }`;
 
 export const NANO_PORT = 7070;
 export const NANO_SERVER = `http://127.0.0.1:${ NANO_PORT }`;
