afa2ead529fbc308f50818b4f5283e6b516ea921
Ty Schenk <ty@brainblocks.io>
2019-03-26 21:16:27 +0000

    Update rateService.js

--- server/lib/rateService.js
+++ server/lib/rateService.js
@@ -54,7 +54,7 @@ export async function getRates() : Promise<Array<{ id : string, price : string, 
     return prices;
 }
 
-async function checkRateArray(item, arr) : Promise<boolean>  {
+export async function checkRateArray(item, arr) : Promise<boolean>  {
     for (let object of arr) {
         if (object.id === item) {
             return true;
